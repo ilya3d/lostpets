fee6b08354d52947a637e43ba39b5d3e9e9952fc
Maxim Kuptsov <maximkuptsov@gmail.com>
2014-12-06 20:17:37 +0300

    fix filter

--- frontend/controllers/MapController.php
+++ frontend/controllers/MapController.php
@@ -20,14 +20,6 @@ class MapController extends Controller
         $filter = new FilterForm();
         $filter->load(Yii::$app->request->post());
 
-      /* $filter->load([
-            'animal'=>[1,2,3],
-            'type'=>[1,2,3],
-            'topleft'=>[0,3],
-            'botright'=>[100,100]
-        ],''); */
-        
-
         if ($filter->validate())
            echo json_encode(Point::findPolygon($filter->topleft,$filter->botright,$filter->type,$filter->animal));
         else echo json_encode(['error'=>[$filter->firstErrors]]);
