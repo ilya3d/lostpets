1fb3d1e19f95bcec20db8b1cc60b86b73056f3a3
ArmiT <artem.rar@gmail.com>
2014-12-07 21:14:44 +0400

    modify print version. added window.print on load event.

--- frontend/views/print/index.php
+++ frontend/views/print/index.php
@@ -1,9 +1,9 @@
-<div class="b-container">
+<div class="b-container ca-paper">
     <div class="logo"><img src="<?= \Yii::$app->getUrlManager()->createAbsoluteUrl('/images/logo.png') ?>"></div>
-    <div class="quercode"><img src="<?= \Yii::$app->getUrlManager()->createAbsoluteUrl('/qr/'.$description->qrcode) ?>"></div>
     <div class="g-clear"></div>
     <div class="b-editor">
         <p class="title"><?= $description->title ?></p>
+        <div class="quercode"><img src="<?= \Yii::$app->getUrlManager()->createAbsoluteUrl('/qr/'.$description->qrcode) ?>"></div>
         <?php if($description->photo): ?>
         <p><img src="<?= \Yii::$app->getUrlManager()->createAbsoluteUrl('/uploads/'.$description->photo) ?>" alt="" /></p>
         <?php endif ?>
