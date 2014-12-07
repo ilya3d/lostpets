809bb666920ba38c1262c2ca6071bd15727b918d
max <maximkuptsov@gmail.com>
2014-12-08 00:36:37 +0300

    detail description

--- frontend/views/description/view.php
+++ frontend/views/description/view.php
@@ -1,13 +1,33 @@
-<div class="b-container">
-    <div class="logo"><img src="<?= \Yii::$app->getUrlManager()->createAbsoluteUrl('/images/logo.png') ?>"></div>
-    <div class="quercode"><img src="<?= \Yii::$app->getUrlManager()->createAbsoluteUrl('/qr/'.$description->qrcode) ?>"></div>
-    <div class="g-clear"></div>
-    <div class="b-editor">
-        <p class="title"><?= $description->title ?></p>
-        <?php if($description->photo): ?>
-            <p><img src="<?= \Yii::$app->getUrlManager()->createAbsoluteUrl('/uploads/'.$description->photo) ?>" alt="" /></p>
-        <?php endif ?>
-        <p><?= $description->description ?></p>
-    </div>
-    <div class="phonebox__item"><span>tel.: <?= $description->phone ?></span></div>
+<? use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+\yii\bootstrap\BootstrapAsset::register($this);  ?>
+
+<div class="description-view">
+    <h1><?= Html::encode($description->title) ?></h1>
+    <?= DetailView::widget([
+        'model' => $description,
+        'attributes' => [
+            [
+                'label'=>'photo',
+                'format'=>'html',
+                'value'=>Html::img('/uploads/'.$description->photo)
+            ],
+            'title',
+            'description',
+            'phone',
+            'email',
+            [
+                'label'=>'print version',
+                'format'=>'html',
+                'value'=>Html::a('link','/print/'.$description->hash)
+            ]
+            /*
+            'photo'=>function(){
+                return ;
+            }*/
+
+        ],
+    ]) ?>
+
 </div>
